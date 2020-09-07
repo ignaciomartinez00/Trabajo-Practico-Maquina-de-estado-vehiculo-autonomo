86152e4ac8b57310df8386da275768444b1f9c83
ignaciomartinez00 <63934479+ignaciomartinez00@users.noreply.github.com>
2020-09-07 15:09:54 -0300

    Update mylib.h

--- mylib.h
+++ mylib.h
@@ -8,22 +8,17 @@
 #include<conio.h>
 #include<string.h>
 
-//prototipo de ARCHIVO
-
-
 char *getkey(char *key);
 void archivo(void);
 
-
-
-typedef enum //en el mismo orden que las f_
+typedef enum
 {
     sistema_central,
-    camara,//funciones inicialiazadoras T2
-    sist_central_velocidades,//funcion inicializadora tipo 3
-    motor,//funciones inicialiazadoras
-    combustible,//funciones inicialiazadoras
-    parabrisas,//funciones inicialiazadoras
+    camara,
+    sist_central_velocidades,
+    motor,
+    combustible,
+    parabrisas,
     bloque_desicion,
     radar,
     bloqueo,
@@ -31,7 +26,7 @@ typedef enum //en el mismo orden que las f_
     luces,
     volante,
     busca_fallas,
-    giroscopio,// hasta aca apuntan son apuntados desde el sistema central
+    giroscopio,
     semaforo,
     estable,
     inestable,
@@ -39,12 +34,11 @@ typedef enum //en el mismo orden que las f_
     insuficiente,
     activado,
     desactivado,
-}estado_p;//es la posicion de los estados
+}estado_p;
 
-//funciones de cada estado
+
 estado_p f_sistema_central(void);
 estado_p f_camara (void);
-
 estado_p f_sist_central_velocidades(void);
 estado_p f_motor (void);
 estado_p f_combustible(void);
@@ -58,14 +52,12 @@ estado_p f_volante(void);
 estado_p f_busca_fallas (void);
 estado_p f_giroscopio(void);
 estado_p f_semaforo(void);
-
 estado_p f_estable(void);
 estado_p f_inestable(void);
 estado_p f_suficiente(void);
 estado_p f_insuficiente(void);
 estado_p f_activado(void);
 estado_p f_desactivado(void);
-
 int angulo;
 
     typedef struct
@@ -95,11 +87,6 @@ int angulo;
         char accion[30];
     }parametros_t;
 
-
 estado_p (*fms[21])(parametros_t);
 estado_p f_lectura (void);
-
-
-
-
 #endif
