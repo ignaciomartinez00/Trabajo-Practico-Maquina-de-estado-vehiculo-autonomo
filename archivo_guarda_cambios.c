7c810e1725c2f11d3ca319b62fa2f6dae0f6acaf
ignaciomartinez00 <63934479+ignaciomartinez00@users.noreply.github.com>
2020-09-28 21:13:51 -0300

    Update archivo_guarda_cambios.c

--- archivo_guarda_cambios.c
+++ archivo_guarda_cambios.c
@@ -3,7 +3,7 @@
 void inicio(void)
 {
     FILE *fp;
-    fp=fopen("parametros.txt","wt");//como la maquina de estado se enciende 1 ves sola y se mantiene encerrado para borrar todo antes de volver a entrar
+    fp=fopen("parametros.txt","wt");
     alta();
 }
 
@@ -25,7 +25,7 @@ void alta(void)
     system("cls");
     fflush(stdin);
     fread(&bf,sizeof(struct guardado),1,fp);
-    while(!feof(fp))  //preguntamos si no es fin de archivo (clase 8)
+    while(!feof(fp))
     {
         if(bf.posicion==posicion)
         {
@@ -34,11 +34,11 @@ void alta(void)
             return;
         }
         fread(&bf,sizeof(struct guardado),1,fp);
-    }//estoy en el final del archivo
+    }
     bf.posicion=posicion;
     bf.variable=valor[posicion];
     fflush(stdin);
-    fwrite(&bf,sizeof(struct guardado),1,fp);//grabo todo lo anterior, solo lo puedo grabar porque tengo el + en rb+. clase 8
+    fwrite(&bf,sizeof(struct guardado),1,fp);
     fclose(fp);
     }
 }
@@ -49,32 +49,32 @@ int recu(int posicion_dentro_del_archivo)
     FILE *fp;
     int valor;
 
-    fp=fopen("parametros.txt","rb+"); //ponerle if y todo eso de validacion
+    fp=fopen("parametros.txt","rb+");
     fread(&bf,sizeof(struct guardado),1,fp);
-    while(!feof(fp))  //preguntamos si no es fin de archivo (clase 8)
+    while(!feof(fp))
     {
         if(bf.posicion==posicion_dentro_del_archivo)
         {
                 valor=bf.variable;
         }
         fread(&bf,sizeof(struct guardado),1,fp);
-    }//estoy en el final del archivo
+    }
     fclose(fp);
     return valor;
 }
 
-int guardar(int valor,int posicion)  //pasar a void
+int guardar(int valor,int posicion)
 {
     struct guardado bf;
     FILE *fp;
-    fp=fopen("parametros.txt","rb+"); //ponerle if y todo eso de validacion
+    fp=fopen("parametros.txt","rb+");
     fread(&bf,sizeof(struct guardado),1,fp);
-    while(!feof(fp))  //preguntamos si no es fin de archivo (clase 8)
+    while(!feof(fp))
     {
         if(bf.posicion==posicion)
         {
             bf.variable=valor;
-            //printf("variable:%04d\tposicion:%04d  \n",bf.variable,bf.posicion); //muestro los datos del legajo a modificar
+            //printf("variable:%04d\tposicion:%04d  \n",bf.variable,bf.posicion);
             fflush(stdin);
             break;
         }
@@ -84,5 +84,5 @@ int guardar(int valor,int posicion)  //pasar a void
     fwrite(&bf,(1L)*sizeof(struct guardado),1,fp);
 
     fclose(fp);
-    return valor;  ///VER si es void
+    return valor;
 }
